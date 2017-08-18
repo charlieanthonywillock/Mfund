59b9f1a064e42408ff6609c4ea1131fd4d0a6f2d
unknown <david.willock@hotmail.com>
2017-08-18 22:13:17 +0100

    ver60

--- src/main/java/com/charlie1/controller/JSONController.java
+++ src/main/java/com/charlie1/controller/JSONController.java
@@ -5,22 +5,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import com.charlie1.funds.model.jRisk;
+import com.charlie1.funds.model.Shop;
 
 @Controller
-@RequestMapping("apiSelectFundsByRisk/")
+@RequestMapping("/kfc/brands")
 public class JSONController {
 
-	@RequestMapping(value = "{json}", method = RequestMethod.GET)
+	@RequestMapping(value = "{name}", method = RequestMethod.GET)
 	public @ResponseBody
-	jRisk getStringJSON(@PathVariable String json) {
+	Shop getShopInJSON(@PathVariable String name) {
 
-		jRisk jrisk = new jRisk();
-		jrisk.setRisk1(json);
-		
-		//jrisk(new String[] { "mkyong1", "mkyong2" });
+		Shop shop = new Shop();
+		shop.setName(name);
+		shop.setStaffName(new String[] { "mkyong1", "mkyong2" });
 
-		return jrisk;
+		return shop;
 
 	}
 
