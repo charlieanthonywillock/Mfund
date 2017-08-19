4aaeb12ddffd1a1cd79784dcbd01ca28ef8f534b
unknown <david.willock@hotmail.com>
2017-08-19 14:23:55 +0100

    added call to gearhost

--- src/main/java/com/charlie1/controller/JSONController.java
+++ src/main/java/com/charlie1/controller/JSONController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.charlie1.funds.dao.FundsDAO;
 import com.charlie1.funds.model.Shop;
 
 @Controller
@@ -14,13 +19,36 @@ public class JSONController {
 	@RequestMapping(value = "{name}", method = RequestMethod.GET)
 	public @ResponseBody
 	Shop getShopInJSON(@PathVariable String name) {
+		
+		
+		 String risk1 = "1";
+    	 String risk2 = "4";
+    	 String str="";
+    	 
+    	 ApplicationContext context = 
+         		new ClassPathXmlApplicationContext("Spring-Module.xml");
+    	 
+    	 
+    	 FundsDAO customerDAO = (FundsDAO) context.getBean("FundsDAO");
+         str =  customerDAO.buildStrPeformanceData(risk1, risk2);
+		
+		
+		
+		
 
 		Shop shop = new Shop();
-		shop.setName(name);
+		shop.setName(str);
 		shop.setStaffName(new String[] { "mkyong1", "mkyong2" });
 
 		return shop;
 
 	}
+	
+	
+	
+	
+	
+	
+	
 
 }
