95e0acf6fe856aeb244fa7114946fb3e724eb322
unknown <david.willock@hotmail.com>
2017-08-22 21:25:51 +0100

    added Test Html Page

---
+++ src/main/java/com/charlie1/controller/FundController.java
@@ -0,0 +1,48 @@
+package com.charlie1.controller;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+
+public class FundController extends AbstractController{
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
+		// TODO Auto-generated method stub
+		
+		
+		ModelAndView model = new ModelAndView("FundPage");
+		model.addObject("msg", "Test Fund");
+
+		return model;
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
