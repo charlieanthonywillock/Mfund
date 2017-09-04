871c208bc79d1319d7ffb9fcbcff4024fbad3ced
unknown <david.willock@hotmail.com>
2017-09-04 22:09:51 +0100

    troubleshooting ajax restful spring post 3

--- src/main/java/com/charlie1/funds/model/jRisk.java
+++ src/main/java/com/charlie1/funds/model/jRisk.java
@@ -23,14 +23,21 @@ public class jRisk {
 	
 	
 
-	public String getRisk() {
+	public String getRisk1() {
 		return risk1;
 	}
 
-	public void setRisk(String risk1) {
+	public void setRisk1(String risk1) {
 		this.risk1 = risk1;
 	}
 
+	public String getRisk2() {
+		return risk2;
+	}
+
+	public void setRisk2(String risk2) {
+		this.risk2 = risk2;
+	}
 	
 	
 	
