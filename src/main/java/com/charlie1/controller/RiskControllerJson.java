2a9fb2d6c0f37a97cb65c9bc8bf9472521e4c0f2
unknown <david.willock@hotmail.com>
2017-09-04 22:39:49 +0100

    troubleshooting ajax restful spring post 4

--- src/main/java/com/charlie1/controller/RiskControllerJson.java
+++ src/main/java/com/charlie1/controller/RiskControllerJson.java
@@ -1,13 +1,17 @@
 package com.charlie1.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
-	import org.springframework.web.bind.annotation.RequestMapping;
-	import org.springframework.web.bind.annotation.RequestParam;
-	import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 	
 	import com.charlie1.funds.model.jRisk;
 
-
+@RequestMapping("/rest/apiCharlie1")
 @RestController
 public class RiskControllerJson {
 	
@@ -17,9 +21,21 @@ public class RiskControllerJson {
 	    private static final String template = "Hello, %s!";
 	    private final AtomicLong counter = new AtomicLong();
 
-	    @RequestMapping("/rest/apiCharlie1")
-	    public jRisk getRisk(@RequestParam(value="name", defaultValue="World") String name) {
-	        return new jRisk("Charlie1",String.format(template, name));
+	    
+	   
+		@RequestMapping(value = "{name}", method = RequestMethod.GET)
+		public @ResponseBody
+		jRisk getTestJSON(@PathVariable String name) {
+			
+	    
+	    
+	    //public jRisk getRisk(@RequestParam(value="{name}", defaultValue="World") String name) {
+	        
+	    	
+	    	
+	    	
+	    	
+	    	return new jRisk("Charlie1",String.format(template, name));
 	    }
 	}
 	
